e967650b4cadb515313d640e384e1ce758b4ed23
JoaquinCastillo12 <joaquin.castilloh12@gmail.com>
2025-06-10 11:06:56 -0500

    fix: update image URLs for product listings to use Google Drive links

--- frontend/frontend/src/pages/Home.jsx
+++ frontend/frontend/src/pages/Home.jsx
@@ -99,21 +99,21 @@ export default function Home() {
   title: "Bombas Centrífugas",
   description: "Ideales para aumentar la presión del agua en toda la casa",
   icon: "lucide:gauge",
-  image: "public/IMAGEN DESTACADA 1.JPG",
+  image: "https://drive.google.com/file/d/1DqgyySK9MNIAd7pECokHYxOPGLboWYo3/view?usp=drive_link",
   categoria: "BOMBAS CENTRIFUGAS"
 },
 {
   title: "Bombas Sumergibles",
   description: "Perfectas para pozos y extracción de agua subterránea",
   icon: "lucide:waves",
-  image: "public/IMAGEN DESTACADA 2.JPG",
+  image: "https://drive.google.com/file/d/1xAYqLD_PSmGOAyPHT_I1jzP8LydvHXL1/view?usp=drive_link",
   categoria: "BOMBAS PARA POZO"
 },
 {
   title: "Sistemas de Presión",
   description: "Mantienen la presión constante en toda la instalación",
   icon: "lucide:activity",
-  image: "public/IMAGEN DESTACADA 3.JPG",
+  image: "https://drive.google.com/file/d/1DjSEOGzHj0DGxamLL6YI8LQoMjH-75tC/view?usp=drive_link",
   categoria: "SISTEMAS HIDRONEUMATICOS"
 }
             ].map((item, index) => (
